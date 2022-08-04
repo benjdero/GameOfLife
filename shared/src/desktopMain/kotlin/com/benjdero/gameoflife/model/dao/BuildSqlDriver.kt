a48dcaa02d132dc29fd1da60d9f73c43e2a9b624
Benjamin Deroche <benjdero@gmail.com>
2022-08-04 23:37:49 +0200

    Do not recreate the database if it already exist on desktop

--- shared/src/desktopMain/kotlin/com/benjdero/gameoflife/model/dao/BuildSqlDriver.kt
+++ shared/src/desktopMain/kotlin/com/benjdero/gameoflife/model/dao/BuildSqlDriver.kt
@@ -2,11 +2,17 @@ package com.benjdero.gameoflife.model.dao
 
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import java.io.File
+
+private const val FILENAME = "database.db"
 
 fun buildSqlDriver(): SqlDriver {
+    val file = File(FILENAME)
+    val databaseExist: Boolean = file.isFile
     val driver = JdbcSqliteDriver(
-        "jdbc:sqlite:database.db"
+        "jdbc:sqlite:$FILENAME"
     )
-    Database.Schema.create(driver)
+    if (!databaseExist)
+        Database.Schema.create(driver)
     return driver
 }
