115fdfeb3760faa29f7afa66caef85f1098c1059
Benjamin Deroche <benjdero@gmail.com>
2024-07-03 18:52:04 +0200

    Target iOS 15.0 min to simplify SwiftUI developments

--- iosApp/iosApp/GameGridView.swift
+++ iosApp/iosApp/GameGridView.swift
@@ -9,38 +9,24 @@ struct GameGridView: View {
     }
 
     var body: some View {
-        if #available(iOS 15.0, *) {
-            Canvas { context, size in
-                let cellWidth: CGFloat = size.width / CGFloat(world.width)
-                let cellHeight: CGFloat = size.height / CGFloat(world.height)
-                let cellSize: Int = .init(min(cellWidth, cellHeight))
+        Canvas { context, size in
+            let cellWidth: CGFloat = size.width / CGFloat(world.width)
+            let cellHeight: CGFloat = size.height / CGFloat(world.height)
+            let cellSize: Int = .init(min(cellWidth, cellHeight))
 
-                let c = context
-                world.forEachIndexed { x, y, cell in
-                    c.fill(
-                        Path(
-                            CGRect(
-                                x: Int(truncating: x) * cellSize,
-                                y: Int(truncating: y) * cellSize,
-                                width: cellSize,
-                                height: cellSize
-                            )
-                        ),
-                        with: .color(Bool(truncating: cell) ? .green : .white)
-                    )
-                }
-            }
-        } else {
-            LazyVGrid(
-                columns: (1 ... world.width).map { _ in
-                    GridItem(.flexible(), spacing: 1)
-                },
-                spacing: 1
-            ) {
-                ForEach(world.toFlatWorld(), id: \.id) { item in
-                    Rectangle()
-                        .fill(item.cell ? .green : .white)
-                }
+            let c = context
+            world.forEachIndexed { x, y, cell in
+                c.fill(
+                    Path(
+                        CGRect(
+                            x: Int(truncating: x) * cellSize,
+                            y: Int(truncating: y) * cellSize,
+                            width: cellSize,
+                            height: cellSize
+                        )
+                    ),
+                    with: .color(Bool(truncating: cell) ? .green : .white)
+                )
             }
         }
     }
